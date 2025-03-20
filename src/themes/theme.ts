095b85c28a38715638c1c7942b5bf9bb74d91f19
longdbs <long.tran@deltabrains.vn>
2025-03-20 16:14:49 +0900

    DBS-002: change color

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -7,14 +7,15 @@ export const theme = createTheme({
       contrastText: "#fff",
     },
     secondary: {
-      main: "#dc004e",
+      main: "#fff",
+      contrastText: "#000",
     },
     error: {
       main: "#f44336",
     },
     background: {
-      default: "#f4f6f8",
-      paper: "#F3F3F3",
+      default: "#fff",
+      paper: "#F6F6F6",
     },
   },
   typography: {
